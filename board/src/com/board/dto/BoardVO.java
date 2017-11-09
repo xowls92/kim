9c929f80d6a97feb663bf33b393ebeaced4c9b27
kim <java-01@java-01-PC>
2017-11-09 17:19:51 +0900

    add BoardVO class

---
+++ board/src/com/board/dto/BoardVO.java
@@ -0,0 +1,24 @@
+package com.board.dto;
+
+public class BoardVO {
+
+	private String bno;
+	private String content;
+
+	public String getBno() {
+		return bno;
+	}
+
+	public void setBno(String bno) {
+		this.bno = bno;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
